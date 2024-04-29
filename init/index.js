255cdc9460a894dff17607ef8ca757900bb18333
NitinParamkar <nitinparamkar@gmail.com>
2024-04-29 18:39:31 +0530

    corrected index.js and data.js files

--- init/index.js
+++ init/index.js
@@ -1,27 +1,52 @@
-//initialization ka logic likh rahe hai.
+let result = require('dotenv').config({ path: 'C:/WanderLust Project/.env' });
+console.log("dotenv result:", result);
+console.log("Environment variables:", process.env);
+
 const mongoose = require("mongoose");                  
 const initData = require("./data.js");
 const Listing = require("../models/listing.js");
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+const multer  = require('multer'); 
+const {storage} = require("../cloudConfig.js");
+const upload = multer({ storage }) ; 
+const cloudinary = require('cloudinary').v2;
 
-main()
-  .then(() => {
-    console.log("connected to DB");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
 
-async function main() {
-  await mongoose.connect(MONGO_URL);
+const dbUrl = process.env.ATLASDB_URL;
+console.log("ATLASDB_URL:", dbUrl); 
+if (!dbUrl) {
+  console.error("ATLASDB_URL is not defined in the environment variables.");
+  process.exit(1); 
 }
 
-const initDB = async () => {
-  await Listing.deleteMany({}); // agar database ke aander pahale se kuch data pada hua hai then usse completely clean kardo
-  initData.data=initData.data.map((obj)=>({...obj,owner:"6606671bb4074cf5da28c71f"}));
-  await Listing.insertMany(initData.data); // data.js se data aa jaye ga.
-  console.log("data was initialized");
-};
+async function main() {
+  try {
+    await mongoose.connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+    console.log("Connected to DB");
 
-initDB();
+    // Initialize cloudinary
+    cloudinary.config({ 
+      cloud_name: process.env.CLOUD_NAME,
+      api_key: process.env.CLOUD_API_KEY,
+      api_secret: process.env.CLOUD_API_SECRET,
+    });
+
+    // Initialize database
+    await initDB();
+  } catch (error) {
+    console.error("Error connecting to DB:", error);
+  }
+}
+
+main();
+
+const initDB = async () => {
+  try {
+    await Listing.deleteMany({});
+    initData.data = initData.data.map((obj) => ({ ...obj, owner: "66194f91591dba604e88bde2" }));
+    await Listing.insertMany(initData.data);
+    console.log("Data was initialized");
+  } catch (error) {
+    console.error("Error initializing data:", error);
+  }
+};
