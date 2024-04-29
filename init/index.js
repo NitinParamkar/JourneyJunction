6b7596edaed031d44c04ed91e9ee7273aab82d8f
NitinParamkar <nitinparamkar@gmail.com>
2024-04-29 23:21:48 +0530

    resolved cropping issue of edit.js file

--- init/index.js
+++ init/index.js
@@ -14,10 +14,10 @@ const cloudinary = require('cloudinary').v2;
 
 const dbUrl = process.env.ATLASDB_URL;
 console.log("ATLASDB_URL:", dbUrl); 
-if (!dbUrl) {
-  console.error("ATLASDB_URL is not defined in the environment variables.");
-  process.exit(1); 
-}
+// if (!dbUrl) {
+//   console.error("ATLASDB_URL is not defined in the environment variables.");
+//   process.exit(1); 
+// }
 
 async function main() {
   try {
@@ -50,3 +50,65 @@ const initDB = async () => {
     console.error("Error initializing data:", error);
   }
 };
+
+
+
+// <---------The following code snippet is utilized to display cropped images upon rendering the edit.js file. --------->
+
+// const initDB = async () => {
+//   try {
+//     //To clear existing listings before inserting new ones
+//     await Listing.deleteMany({});
+
+//     // Map over the initData data and add the owner field
+//     initData.data = initData.data.map((obj) => ({ ...obj, owner:  process.env.ATLAS_ADMIN }));
+
+//     // Iterate through the initData data and upload images to Cloudinary
+//     const listingsWithCloudinaryImages = await Promise.all(
+//       initData.data.map(async (listingData) => {
+//         // Extract the image data from the listingData
+//         const { image, ...rest } = listingData;
+
+//         try {
+//           // Ensure that `image` is structured as expected
+//           const imageData = image && image.url ? image.url : null;
+
+//           if (!imageData) {
+//             throw new Error('Image data is missing or invalid.');
+//           }
+
+//           // Use Cloudinary's `upload` function to upload the image directly
+//           const cloudinaryImage = await cloudinary.uploader.upload(imageData, {
+//             folder: 'wanderlust_DEV',
+//             allowed_formats: ["png", "jpg", "jpeg"],
+//           });
+
+//           // Create a new listing object with Cloudinary image URL
+//           const listingWithImage = {
+//             ...rest,
+//             image: {
+//               url: cloudinaryImage.url,
+//               filename: cloudinaryImage.original_filename,
+//             },
+//             owner: process.env.ATLAS_ADMIN,
+//           };
+
+//           return listingWithImage;
+//         } catch (uploadError) {
+//           console.error('Error uploading image to Cloudinary:', uploadError);
+//           throw uploadError; // Rethrow the error to propagate it to the outer catch block
+//         }
+//       })
+//     );
+
+//     // Insert the listings with Cloudinary images into the database
+//     let listings = await Listing.insertMany(listingsWithCloudinaryImages);
+
+//     console.log("Data was initialized with Cloudinary images");
+//   } catch (error) {
+//     console.error('Error in initDB:', error);
+//   }
+// };
+
+
+// initDB();
